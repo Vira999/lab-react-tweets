8116290b564717a72d2a9f0a78f7cc77d355538b
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-01-06 21:00:00 +0100

    Add files via upload

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function ProfileImage(props) {
 
     const {image} = props
     return (
         <div>
-            <img src={image} className="profile" alt="profileimage" />
+            <img src={image} className="profile" alt="profileimage" key={uuidv4()} />
 </div> )}
 
 export default ProfileImage
--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function Timestamp(props) {
     const {timestamp} = props
     return (
-        <div> <span className="timestamp">{timestamp}</span></div> )}
+        <div> <span className="timestamp" key={uuidv4()}>{timestamp}</span></div> )}
 
 export default Timestamp
